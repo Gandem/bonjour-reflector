532bf8b4c0c982009efe6daef3a439026df3e6ed
Nayef Ghattas <nayef.ghattas@gmail.com>
2018-02-06 00:06:17 +0100

    dep: switch to gopacket <3

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
+	"time"
 
-	"github.com/songgao/ether"
-	"github.com/songgao/packets/ethernet"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 func main() {
@@ -15,18 +16,35 @@ func main() {
 		log.Fatalf("Could not read configuration : %v", err)
 	}
 
-	netInterface, err := net.InterfaceByName(cfg.NetInterface)
-	if err != nil {
-		log.Fatalf("Could not list interfaces: %s", err)
+	src, err := pcap.OpenLive(cfg.NetInterface, 65536, true, time.Second)
+
+	var dec gopacket.Decoder
+	var ok bool
+
+	if dec, ok = gopacket.DecodersByLayerName["Ethernet"]; !ok {
+		log.Fatalln("No decoder named Ethernet")
 	}
+	source := gopacket.NewPacketSource(src, dec)
 
-	netDevice, err := ether.NewDev(netInterface, nil)
-	if err != nil {
-		log.Fatalf("Could not tap to interface: %s", err)
+	var eth layers.Ethernet
+	var ip4 layers.IPv4
+	var ip6 layers.IPv6
+	var tcp layers.TCP
+	var tag layers.Dot1Q
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &tag, &eth, &ip4, &ip6, &tcp)
+	decoded := []gopacket.LayerType{}
+
+	for packet := range source.Packets() {
+		parser.DecodeLayers(packet.Data(), &decoded)
+		for _, layerType := range decoded {
+			switch layerType {
+			case layers.LayerTypeDot1Q:
+				fmt.Println(tag.VLANIdentifier)
+			case layers.LayerTypeEthernet:
+				fmt.Printf("Source MAC: %v, Dest MAC: %v \n", eth.DstMAC, eth.SrcMAC)
+			case layers.LayerTypeIPv4:
+				fmt.Printf("Source IP: %v, Dest IP: %v \n", ip4.SrcIP, ip4.DstIP)
+			}
+		}
 	}
-
-	var res ethernet.Frame
-	netDevice.Read(&res)
-
-	fmt.Println(res)
 }
