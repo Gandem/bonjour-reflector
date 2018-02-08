1fce040c9f3f7428a4adf85145e2fe5adc51799d
Anatole Beuzon <anatole.beuzon@student.ecp.fr>
2018-02-08 22:14:07 +0100

    fix: adapt tests to new config scheme

--- config_test.go
+++ config_test.go
@@ -3,47 +3,32 @@ package main
 import (
 	"reflect"
 	"testing"
+	"sort"
 )
 
-func TestAppendWithoutDuplicates(t *testing.T) {
-  array1, array2 := []int{1, 2, 3}, []int{4, 5, 6, 3, 7}
-  computedResult := appendWithoutDuplicates(array1, array2)
-  expectedResult := []int{1, 2, 3, 4, 5, 6, 7}
-  if !reflect.DeepEqual(computedResult, expectedResult) {
-    t.Error("Error in appendWithoutDuplicates()")
-  }
-}
-
-var devices = []bonjourDevice{
-  bonjourDevice{MacAddress: "00:14:22:01:23:45", Pools: []int{42, 1042, 1337}},
-  bonjourDevice{MacAddress: "00:14:22:01:23:46", Pools: []int{176, 148}},
-  bonjourDevice{MacAddress: "00:14:22:01:23:47", Pools: []int{1042, 1717, 13}},
+var devices = map[macAddress]bonjourDevice{
+  "00:14:22:01:23:45": bonjourDevice{OriginPool: 45, SharedPools: []int{42, 1042, 46}},
+  "00:14:22:01:23:46": bonjourDevice{OriginPool: 46, SharedPools: []int{176, 148}},
+  "00:14:22:01:23:47": bonjourDevice{OriginPool: 47, SharedPools: []int{1042, 1717, 13}},
 }
 
 func TestMapByPool(t *testing.T) {
   computedResult := mapByPool(devices)
+	// Sort slices to ensure that a different order does not make the test fail
+	for _, slice := range computedResult {
+		sort.Ints(slice)
+	}
+
   expectedResult := map[int]([]int){
-    42: []int{1042, 1337},
-    1042: []int{42, 1337, 1717, 13},
-    1337: []int{42, 1042},
-    176: []int{148},
-    148: []int{176},
-    1717: []int{1042, 13},
-    13: []int{1042, 1717},
+    42: []int{45},
+    1042: []int{45, 47},
+    46: []int{45},
+    176: []int{46},
+    148: []int{46},
+		13: []int{47},
+    1717: []int{47},
   }
   if !reflect.DeepEqual(computedResult, expectedResult) {
     t.Error("Error in mapByPool()")
   }
-}
-
-func TestMapByAddress(t *testing.T) {
-	computedResult := mapByAddress(devices)
-	expectedResult := map[string]([]int){
-    "00:14:22:01:23:45": []int{42, 1042, 1337},
-    "00:14:22:01:23:46": []int{176, 148},
-    "00:14:22:01:23:47": []int{1042, 1717, 13},
-  }
-	if !reflect.DeepEqual(computedResult, expectedResult) {
-		t.Error("Error in mapByAddress()")
-	}
 }
