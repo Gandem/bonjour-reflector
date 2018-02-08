dc453ab23f1a57a57b77343692a88c9014709c2a
Nayef Ghattas <nayef.ghattas@gmail.com>
2018-02-08 13:28:40 +0100

    Implement config file structure for device/vlan sharing (#3)
    * feat: implement config and specific config views
    
    * feat: add another config view
    
    * refactor: change config file structure
    
    * refactor: config to a map indexed by mac address

--- config.go
+++ config.go
@@ -6,8 +6,16 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type macAddress string
+
 type brconfig struct {
-	NetInterface string `toml:"net_interface"`
+	NetInterface string                       `toml:"net_interface"`
+	Devices      map[macAddress]bonjourDevice `toml:"devices"`
+}
+
+type bonjourDevice struct {
+	OriginPool  int   `toml:"origin_pool"`
+	SharedPools []int `toml:"shared_pools"`
 }
 
 func readConfig(path string) (cfg brconfig, err error) {
@@ -17,4 +25,21 @@ func readConfig(path string) (cfg brconfig, err error) {
 	}
 	_, err = toml.Decode(string(content), &cfg)
 	return cfg, err
+}
+
+func mapByPool(devices map[macAddress]bonjourDevice) map[int]([]int) {
+	seen := make(map[int]map[int]bool)
+	poolsMap := make(map[int]([]int))
+	for _, device := range devices {
+		for _, pool := range device.SharedPools {
+			if _, ok := seen[pool]; !ok {
+				seen[pool] = make(map[int]bool)
+			}
+			if _, ok := seen[pool][device.OriginPool]; !ok {
+				seen[pool][device.OriginPool] = true
+				poolsMap[pool] = append(poolsMap[pool], device.OriginPool)
+			}
+		}
+	}
+	return poolsMap
 }
