5e89c33f1a276362cb24b68c00ef9f0cefbaab3a
Anatole Beuzon <beuzon.a@gmail.com>
2018-02-11 17:43:20 +0100

    fix: failing config test because of pool type change

--- config_test.go
+++ config_test.go
@@ -2,33 +2,33 @@ package main
 
 import (
 	"reflect"
-	"testing"
 	"sort"
+	"testing"
 )
 
 var devices = map[macAddress]bonjourDevice{
-  "00:14:22:01:23:45": bonjourDevice{OriginPool: 45, SharedPools: []int{42, 1042, 46}},
-  "00:14:22:01:23:46": bonjourDevice{OriginPool: 46, SharedPools: []int{176, 148}},
-  "00:14:22:01:23:47": bonjourDevice{OriginPool: 47, SharedPools: []int{1042, 1717, 13}},
+	"00:14:22:01:23:45": bonjourDevice{OriginPool: 45, SharedPools: []uint16{42, 1042, 46}},
+	"00:14:22:01:23:46": bonjourDevice{OriginPool: 46, SharedPools: []uint16{176, 148}},
+	"00:14:22:01:23:47": bonjourDevice{OriginPool: 47, SharedPools: []uint16{1042, 1717, 13}},
 }
 
 func TestMapByPool(t *testing.T) {
-  computedResult := mapByPool(devices)
+	computedResult := mapByPool(devices)
 	// Sort slices to ensure that a different order does not make the test fail
 	for _, slice := range computedResult {
-		sort.Ints(slice)
+		sort.Slice(slice, func(i, j int) bool { return slice[i] < slice[j] })
 	}
 
-  expectedResult := map[int]([]int){
-    42: []int{45},
-    1042: []int{45, 47},
-    46: []int{45},
-    176: []int{46},
-    148: []int{46},
-		13: []int{47},
-    1717: []int{47},
-  }
-  if !reflect.DeepEqual(computedResult, expectedResult) {
-    t.Error("Error in mapByPool()")
-  }
+	expectedResult := map[uint16]([]uint16){
+		42:   []uint16{45},
+		1042: []uint16{45, 47},
+		46:   []uint16{45},
+		176:  []uint16{46},
+		148:  []uint16{46},
+		13:   []uint16{47},
+		1717: []uint16{47},
+	}
+	if !reflect.DeepEqual(computedResult, expectedResult) {
+		t.Error("Error in mapByPool()")
+	}
 }
