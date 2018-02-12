7c0f30c2c7b5c78e91189da3ab40d893a582d117
Anatole Beuzon <beuzon.a@gmail.com>
2018-02-12 09:54:51 +0100

    minor: add comments

--- main.go
+++ main.go
@@ -13,33 +13,36 @@ import (
 const configPath = "./config.toml"
 
 func main() {
+	// Read config file and generate mDNS forwarding maps
 	cfg, err := readConfig(configPath)
 	if err != nil {
 		log.Fatalf("Could not read configuration: %v", err)
 	}
 	poolsMap := mapByPool(cfg.Devices)
 
+	// Get a handle on the network interface
 	rawTraffic, err := pcap.OpenLive(cfg.NetInterface, 65536, true, time.Second)
 	if err != nil {
 		log.Fatalf("Could not find network interface: %v", cfg.NetInterface)
 	}
 
-	// Get the Mac Address to filter for Bonjour packet duplicates
+	// Get the local MAC address, to filter out Bonjour packet generated locally
 	intf, err := net.InterfaceByName(cfg.NetInterface)
 	if err != nil {
 		log.Fatal(err)
 	}
 	brMACAddress := intf.HardwareAddr
 
+	// Get a channel of Bonjour packets to process
 	decoder := gopacket.DecodersByLayerName["Ethernet"]
-
 	source := gopacket.NewPacketSource(rawTraffic, decoder)
-
 	bonjourPackets := filterBonjourPacketsLazily(source, brMACAddress)
 
+	// Process Bonjours packets
 	for bonjourPacket := range bonjourPackets {
 		fmt.Println(bonjourPacket.packet.String())
 		if bonjourPacket.vlanTag != nil {
+			// Forward the mDNS query or response to appropriate VLANs
 			if bonjourPacket.isDNSQuery {
 				if tags, ok := poolsMap[*bonjourPacket.vlanTag]; ok {
 					for _, tag := range tags {
