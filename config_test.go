99b4914fb6233fa3f260a14cbc918fa6c8bdffb7
Anatole Beuzon <oxlay@users.noreply.github.com>
2019-01-21 18:53:57 -0500

    Improve test coverage (#19)
    * feature: add tests for readConfig()
    
    * fix: error message in config_test
    
    * feature: add preliminary test for sendBonjourPacket()
    
    * fix: temporarily comment out failed test
    
    * feature: improve coverage of filterBonjourPacketsLazily()
    
    * refactor TestSendBonjourPacket()
    
    * fix: test for sendBonjourPacket()
    
    * fix: outdated Gopkg.lock
    
    * minor: improve syntax in TestReadConfig

--- config_test.go
+++ config_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"reflect"
 	"sort"
 	"testing"
@@ -10,6 +11,33 @@ var devices = map[macAddress]bonjourDevice{
 	"00:14:22:01:23:45": bonjourDevice{OriginPool: 45, SharedPools: []uint16{42, 1042, 46}},
 	"00:14:22:01:23:46": bonjourDevice{OriginPool: 46, SharedPools: []uint16{176, 148}},
 	"00:14:22:01:23:47": bonjourDevice{OriginPool: 47, SharedPools: []uint16{1042, 1717, 13}},
+}
+
+func TestReadConfig(t *testing.T) {
+	// Check that a valid config file is read adequately
+	validTestConfigFile := "config_test.toml"
+	computedCfg, err := readConfig(validTestConfigFile)
+	expectedCfg := brconfig{
+		NetInterface: "test0",
+		Devices:      devices,
+	}
+
+	if err != nil {
+		t.Errorf("Error in readConfig(): failed to read test config file %s", validTestConfigFile)
+	} else if !reflect.DeepEqual(expectedCfg, computedCfg) {
+		t.Error("Error in readConfig(): expected config does not match computed config")
+	}
+
+	// Check that a non-existant config file is handled adequately
+	nonexistantConfigFile := "nonexistant_test.toml"
+	computedCfg, err = readConfig(nonexistantConfigFile)
+	if !reflect.DeepEqual(computedCfg, brconfig{}) {
+		t.Error("Error in readConfig(): unexpected config returned for non-existant config file")
+	}
+	if !os.IsNotExist(err) {
+		// if the error returned is not of type "file not found"
+		t.Error("Error in readConfig(): wrong error returned for nonexistant config file")
+	}
 }
 
 func TestMapByPool(t *testing.T) {
--- packet_test.go
+++ packet_test.go
@@ -18,30 +18,40 @@ var (
 	vlanIdentifierTest = uint16(30)
 	srcIPv4Test        = net.IP{127, 0, 0, 1}
 	dstIPv4Test        = net.IP{224, 0, 0, 251}
+	dstIPv4ToIgnore    = net.IP{224, 0, 0, 252}
 	srcIPv6Test        = net.ParseIP("::1")
 	dstIPv6Test        = net.ParseIP("ff02::fb")
+	dstIPv6ToIgnore    = net.ParseIP("ff02::fc")
 	srcUDPPortTest     = layers.UDPPort(5353)
 	dstUDPPortTest     = layers.UDPPort(5353)
+	dstUDPPortToIgnore = layers.UDPPort(5352)
 )
 
 func createMockmDNSPacket(isIPv4 bool, isDNSQuery bool) []byte {
+	if isIPv4 {
+		return createRawPacket(isIPv4, isDNSQuery, vlanIdentifierTest, dstIPv4Test, srcMACTest, dstMACTest, dstUDPPortTest)
+	}
+	return createRawPacket(isIPv4, isDNSQuery, vlanIdentifierTest, dstIPv6Test, srcMACTest, dstMACTest, dstUDPPortTest)
+}
+
+func createRawPacket(isIPv4 bool, isDNSQuery bool, vlanTag uint16, dstIP net.IP, srcMAC net.HardwareAddr, dstMAC net.HardwareAddr, dstPort layers.UDPPort) []byte {
 	var ethernetLayer, dot1QLayer, ipLayer, udpLayer, dnsLayer gopacket.SerializableLayer
 
 	ethernetLayer = &layers.Ethernet{
-		SrcMAC:       srcMACTest,
-		DstMAC:       dstMACTest,
+		SrcMAC:       srcMAC,
+		DstMAC:       dstMAC,
 		EthernetType: layers.EthernetTypeDot1Q,
 	}
 
 	if isIPv4 {
 		dot1QLayer = &layers.Dot1Q{
-			VLANIdentifier: vlanIdentifierTest,
+			VLANIdentifier: vlanTag,
 			Type:           layers.EthernetTypeIPv4,
 		}
 
 		ipLayer = &layers.IPv4{
 			SrcIP:    srcIPv4Test,
-			DstIP:    dstIPv4Test,
+			DstIP:    dstIP,
 			Version:  4,
 			Protocol: layers.IPProtocolUDP,
 			Length:   146,
@@ -50,13 +60,13 @@ func createMockmDNSPacket(isIPv4 bool, isDNSQuery bool) []byte {
 		}
 	} else {
 		dot1QLayer = &layers.Dot1Q{
-			VLANIdentifier: vlanIdentifierTest,
+			VLANIdentifier: vlanTag,
 			Type:           layers.EthernetTypeIPv6,
 		}
 
 		ipLayer = &layers.IPv6{
 			SrcIP:      srcIPv6Test,
-			DstIP:      dstIPv6Test,
+			DstIP:      dstIP,
 			Version:    6,
 			Length:     48,
 			NextHeader: layers.IPProtocolUDP,
@@ -65,7 +75,7 @@ func createMockmDNSPacket(isIPv4 bool, isDNSQuery bool) []byte {
 
 	udpLayer = &layers.UDP{
 		SrcPort: srcUDPPortTest,
-		DstPort: dstUDPPortTest,
+		DstPort: dstPort,
 	}
 
 	if isDNSQuery {
@@ -194,37 +204,45 @@ func TestParseDNSPayload(t *testing.T) {
 }
 
 type dataSource struct {
-	packetSent bool
-	data       []byte
+	sentPackets int
+	data        [][]byte
 }
 
 func (dataSource *dataSource) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err error) {
-	// Return one packet.
-	// If a packet has already been returned in the past, return an EOF error
+	// Return one packet for each call.
+	// If all the expected packets have already been returned in the past, return an EOF error
 	// to end the reading of packets from this source.
-	data = dataSource.data
+	if dataSource.sentPackets >= len(dataSource.data) {
+		return nil, ci, io.EOF
+	}
+	data = dataSource.data[dataSource.sentPackets]
 	ci = gopacket.CaptureInfo{
 		Timestamp:      time.Time{},
 		CaptureLength:  len(data),
 		Length:         ci.CaptureLength,
 		InterfaceIndex: 0,
 	}
-	if !dataSource.packetSent {
-		dataSource.packetSent = true
-		return data, ci, nil
-	}
-	return nil, ci, io.EOF
+	dataSource.sentPackets++
+	return data, ci, nil
 }
 
 func createMockPacketSource() (packetSource *gopacket.PacketSource, packet gopacket.Packet) {
-	data := createMockmDNSPacket(true, true)
+	// First, send packets that should be filtered
+	// Then, send one legitimate packet
+	// Return the packetSource and the legitimate packet
+	data := [][]byte{
+		createRawPacket(true, true, vlanIdentifierTest, dstIPv4ToIgnore, srcMACTest, dstMACTest, dstUDPPortTest),
+		createRawPacket(false, true, vlanIdentifierTest, dstIPv6ToIgnore, srcMACTest, dstMACTest, dstUDPPortTest),
+		createRawPacket(true, true, vlanIdentifierTest, dstIPv4Test, brMACTest, dstMACTest, dstUDPPortTest),
+		createRawPacket(true, true, vlanIdentifierTest, dstIPv4Test, srcMACTest, dstMACTest, dstUDPPortToIgnore),
+		createMockmDNSPacket(true, true)}
 	dataSource := &dataSource{
-		packetSent: false,
-		data:       data,
+		sentPackets: 0,
+		data:        data,
 	}
 	decoder := gopacket.DecodersByLayerName["Ethernet"]
 	packetSource = gopacket.NewPacketSource(dataSource, decoder)
-	packet = gopacket.NewPacket(data, decoder, gopacket.DecodeOptions{Lazy: true})
+	packet = gopacket.NewPacket(data[len(data)-1], decoder, gopacket.DecodeOptions{Lazy: true})
 	return
 }
 
@@ -251,5 +269,66 @@ func TestFilterBonjourPacketsLazily(t *testing.T) {
 	computedResult := <-packetChan
 	if !areBonjourPacketsEqual(expectedResult, computedResult) {
 		t.Error("Error in filterBonjourPacketsLazily()")
+	}
+}
+
+type mockPacketWriter struct {
+	packet gopacket.Packet
+}
+
+func (pw *mockPacketWriter) WritePacketData(bytes []byte) (err error) {
+	decoder := gopacket.DecodersByLayerName["Ethernet"]
+	pw.packet = gopacket.NewPacket(bytes, decoder, gopacket.DecodeOptions{Lazy: true})
+	return
+}
+
+func TestSendBonjourPacket(t *testing.T) {
+	// Craft a test packet
+	initialDataIPv4 := createMockmDNSPacket(true, true)
+	initialDataIPv6 := createMockmDNSPacket(false, true)
+	decoder := gopacket.DecodersByLayerName["Ethernet"]
+	initialPacketIPv4 := gopacket.NewPacket(initialDataIPv4, decoder, gopacket.DecodeOptions{Lazy: true})
+	initialPacketIPv6 := gopacket.NewPacket(initialDataIPv6, decoder, gopacket.DecodeOptions{Lazy: true})
+
+	srcMACv4, dstMACv4 := parseEthernetLayer(initialPacketIPv4)
+	bonjourTestPacketIPv4 := bonjourPacket{
+		packet:     initialPacketIPv4,
+		vlanTag:    parseVLANTag(initialPacketIPv4),
+		srcMAC:     srcMACv4,
+		dstMAC:     dstMACv4,
+		isDNSQuery: true,
+		isIPv6:     false,
+	}
+
+	srcMACv6, dstMACv6 := parseEthernetLayer(initialPacketIPv6)
+	bonjourTestPacketIPv6 := bonjourPacket{
+		packet:     initialPacketIPv6,
+		vlanTag:    parseVLANTag(initialPacketIPv6),
+		srcMAC:     srcMACv6,
+		dstMAC:     dstMACv6,
+		isDNSQuery: true,
+		isIPv6:     true,
+	}
+
+	newVlanTag := uint16(29)
+
+	expectedDstMACv4 := net.HardwareAddr{0x01, 0x00, 0x5E, 0x00, 0x00, 0xFB}
+	expectedDataIPv4 := createRawPacket(true, true, newVlanTag, dstIPv4Test, brMACTest, expectedDstMACv4, dstUDPPortTest)
+	expectedPacketIPv4 := gopacket.NewPacket(expectedDataIPv4, decoder, gopacket.DecodeOptions{Lazy: true})
+
+	expectedDstMACv6 := net.HardwareAddr{0x33, 0x33, 0x00, 0x00, 0x00, 0xFB}
+	expectedDataIPv6 := createRawPacket(false, true, newVlanTag, dstIPv6Test, brMACTest, expectedDstMACv6, dstUDPPortTest)
+	expectedPacketIPv6 := gopacket.NewPacket(expectedDataIPv6, decoder, gopacket.DecodeOptions{Lazy: true})
+
+	pw := &mockPacketWriter{packet: nil}
+
+	sendBonjourPacket(pw, &bonjourTestPacketIPv4, newVlanTag, brMACTest)
+	if !reflect.DeepEqual(expectedPacketIPv4.Layers(), pw.packet.Layers()) {
+		t.Error("Error in sendBonjourPacket() for IPv4")
+	}
+
+	sendBonjourPacket(pw, &bonjourTestPacketIPv6, newVlanTag, brMACTest)
+	if !reflect.DeepEqual(expectedPacketIPv6.Layers(), pw.packet.Layers()) {
+		t.Error("Error in sendBonjourPacket() for IPv6")
 	}
 }
